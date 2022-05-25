30d0ab598397b002f964835279d65c4e882b4294
Taoist-G <12568507452qq.com>
2022-05-25 19:30:43 +0800

    测试

--- ChessDemo/src/Main.java
+++ ChessDemo/src/Main.java
@@ -1,11 +1,9 @@
 import view.ChessGameFrame;
 
 import javax.swing.*;
-
+//123
 public class Main {
     public static void main(String[] args) {
-        System.out.println("x");
-        System.out.println("y");
         SwingUtilities.invokeLater(() -> {
             ChessGameFrame mainFrame = new ChessGameFrame(1000, 760);
             mainFrame.setVisible(true);
