991aec69b532bf16946e8a75bfd4aa2af1d63712
Taoist-G <12568507452qq.com>
2022-05-26 13:39:23 +0800

    逻辑

--- ChessDemo/src/Main.java
+++ ChessDemo/src/Main.java
@@ -3,6 +3,7 @@ import view.ChessGameFrame;
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
+        //123
         SwingUtilities.invokeLater(() -> {
             ChessGameFrame mainFrame = new ChessGameFrame(1000, 760);
             mainFrame.setVisible(true);
