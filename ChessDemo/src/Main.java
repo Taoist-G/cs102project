33832fad06f72dc54dc58a990228406c47fd29be
Taoist-G <12568507452qq.com>
2022-05-27 11:21:15 +0800

    更新

--- ChessDemo/src/Main.java
+++ ChessDemo/src/Main.java
@@ -1,13 +1,34 @@
 import view.ChessGameFrame;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Music music = new Music();
-            music.playMusic();
-            ChessGameFrame mainFrame = new ChessGameFrame(1000, 760);
-            mainFrame.setVisible(true);
+            ChessGameFrame mainFrame = new  ChessGameFrame(1000, 760);
+            JFrame HomeFrame=new JFrame("Home");
+            JPanel HomePanel = new JPanel(null);
+            HomeFrame.setVisible(true);
+            HomeFrame.setContentPane(HomePanel);
+            HomeFrame.setSize(800,800);
+            HomeFrame.setLocationRelativeTo(null);
+            HomeFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            JButton startGame=new JButton("Start Game");
+            HomePanel.add(startGame);
+            startGame.setSize(200,200);
+            startGame.setBounds(10,10,200,200);
+            startGame.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    HomeFrame.dispose();
+                    mainFrame.setVisible(true);
+                    music.playMusic();
+                }
+            });
+
         });
     }
 }
--- ChessDemo/src/view/ChessGameFrame.java
+++ ChessDemo/src/view/ChessGameFrame.java
@@ -37,7 +37,6 @@ public class ChessGameFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //设置程序关闭按键，如果点击右上方的叉就游戏全部关闭了
 //        setLayout(null);
 
-        setVisible(true);
 
         addLabel();
         addChessboard();
